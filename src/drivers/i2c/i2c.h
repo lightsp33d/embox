c8b186b35ed68b4fe0e8503390204d38746f2cb9
Anton Bondarev <anton.bondarev2310@gmail.com>
2018-11-27 17:59:49 +0300

    drivers: Enable I2C subsystem

--- src/drivers/i2c/i2c.h
+++ src/drivers/i2c/i2c.h
@@ -1,27 +1,64 @@
 #ifndef DRIVERS_I2C_H_
 #define DRIVERS_I2C_H_
 
-struct i2c_device {
-	struct i2c_device *next;
+#include <stdint.h>
+
+#include <util/dlist.h>
+#include <framework/mod/options.h>
+#include <config/embox/driver/i2c.h>
+
+#define I2C_BUS_MAX   \
+	OPTION_MODULE_GET(embox__driver__i2c, NUMBER, i2c_bus_max)
+
+#define MAX_I2C_BUS_NAME    8
+
+struct i2c_algorithm;
+
+struct i2c_msg {
+	uint16_t addr;	/* slave address			*/
+	uint16_t flags;
+#define I2C_M_RD		0x0001	/* read data, from slave to master */
+					/* I2C_M_RD is guaranteed to be 0x0001! */
+#define I2C_M_TEN		0x0010	/* this is a ten bit chip address */
+#define I2C_M_RECV_LEN		0x0400	/* length will be first received byte */
+#define I2C_M_NO_RD_ACK		0x0800	/* if I2C_FUNC_PROTOCOL_MANGLING */
+#define I2C_M_IGNORE_NAK	0x1000	/* if I2C_FUNC_PROTOCOL_MANGLING */
+#define I2C_M_REV_DIR_ADDR	0x2000	/* if I2C_FUNC_PROTOCOL_MANGLING */
+#define I2C_M_NOSTART		0x4000	/* if I2C_FUNC_NOSTART */
+#define I2C_M_STOP		0x8000	/* if I2C_FUNC_PROTOCOL_MANGLING */
+	uint16_t len;		/* msg length				*/
+	uint8_t *buf;		/* pointer to msg data			*/
 };
 
 struct i2c_adapter {
-	void *priv;
+	void *i2c_algo_data;
+	const struct i2c_algorithm *i2c_algo; /* the algorithm to access the bus */
+};
+
+struct i2c_algorithm {
+	int (*i2c_master_xfer)(struct i2c_adapter *adap, struct i2c_msg *msgs,
+			int num);
+	/* To determine what the adapter supports */
+	uint32_t (*i2c_functionality) (struct i2c_adapter *);
 };
 
 struct i2c_bus {
 	int id;
-	int free_entry;
-	struct i2c_adapter *adapter;
-	struct i2c_device devices;
+	char name[MAX_I2C_BUS_NAME];
+
+	struct i2c_adapter *i2c_adapter;
+
+	struct dlist_head i2c_bus_list;
 };
 
-int register_i2c_bus(struct i2c_adapter*);
-int unregister_i2c_bus(int bus_id);
-struct i2c_bus* get_i2c_bus(int);
-int enumerate_i2c_buses(int (*)(int, void*), void*);
+extern int i2c_bus_register(struct i2c_adapter *adap, int id, const char *bus_name);
 
-int register_i2c_device(int, struct i2c_device*);
-int enumerate_i2c_devices(int, int (*)(struct i2c_device*, void*), void*);
+extern int i2c_bus_unregister(int bus_id);
+
+extern struct i2c_bus *i2c_bus_get(int id);
+
+extern int i2c_bus_read(int id, uint16_t addr, uint8_t *ch, size_t sz);
+
+extern int i2c_bus_write(int id, uint16_t addr, uint8_t *ch, size_t sz);
 
 #endif
