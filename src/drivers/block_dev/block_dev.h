bb9f93a031442bd6b271e4abb79646bb8b1e0b0b
Mark Kovalev <restonich@gmail.com>
2019-01-22 16:33:56 +0300

    drivers: (dvfs) remove file_operations from block_dev

--- src/drivers/block_dev/block_dev.h
+++ src/drivers/block_dev/block_dev.h
@@ -25,7 +25,7 @@
 
 struct file_operations;
 typedef struct block_dev {
-	struct file_operations *dev_ops;
+	// struct file_operations *dev_ops;
 	dev_t id;
 	char name[NAME_MAX + 1];
 	void *dev_vfs_info;
